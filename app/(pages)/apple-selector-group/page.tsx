105991f670748d2049ac629c07e0abdec5408ab8
Luther Tchofo Safo <nintendobserver@gmail.com>
2024-06-27 12:36:15 +0200

    Ready for lesson 2.

---
+++ app/(pages)/apple-selector-group/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function AppleSelectorGroupPage() {
+  return (
+    <main className="flex h-screen items-center justify-center bg-black text-white">
+      <div>Hello BuildUI!</div>
+    </main>
+  );
+}
