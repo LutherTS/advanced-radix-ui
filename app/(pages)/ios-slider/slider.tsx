807b5c6a5e5257b19c9f551ca3359c690153af76
Luther Tchofo Safo <nintendobserver@gmail.com>
2024-06-29 12:14:51 +0200

    I honestly should chill I did a great job. In front even. :D

--- app/(pages)/ios-slider/slider.tsx
+++ app/(pages)/ios-slider/slider.tsx
@@ -2,22 +2,32 @@
 
 import * as Slider from "@radix-ui/react-slider";
 import * as Icons from "./icons";
+import { useState } from "react";
 
 export default function SliderComponent({ name }: { name?: string }) {
+  let [isGrabbing, setIsGrabbing] = useState(false);
+
   return (
-    <div className="group flex items-center gap-x-3 transition-[margin] duration-[350ms] *:duration-[350ms] hover:-mx-3 hover:cursor-grab active:cursor-grabbing">
-      <Icons.SpeakerXMark className="size-5 text-gray-300 transition group-hover:scale-125 group-hover:text-white" />
-      <Slider.Root
-        name={name}
-        defaultValue={[50]}
-        className="relative flex h-1.5 grow items-center transition-[height] group-hover:h-4"
-      >
-        <Slider.Track className="relative h-full grow overflow-hidden rounded-full bg-gray-700 transition-colors">
-          <Slider.Range className="absolute h-full bg-gray-300 transition-colors group-hover:bg-white" />
-        </Slider.Track>
-        <Slider.Thumb className="opacity-0" />
-      </Slider.Root>
-      <Icons.SpeakerWave className="size-5 text-gray-300 transition group-hover:scale-125 group-hover:text-white" />
+    <div className="flex h-5 flex-col justify-center">
+      <div className="group flex items-center gap-x-3 duration-[350ms] *:duration-[350ms]">
+        <Icons.SpeakerXMark className="size-[25px] text-gray-400 hover:!text-white group-hover:text-gray-200 group-hover:transition" />
+        <Slider.Root
+          name={name}
+          defaultValue={[50]}
+          className="relative flex h-2 grow items-center transition-[height] hover:h-5"
+        >
+          <Slider.Track
+            className="relative h-full grow overflow-hidden rounded-full bg-gray-700 *:bg-gray-400 *:transition-colors *:hover:!bg-white *:group-hover:bg-gray-200"
+            style={{ cursor: isGrabbing ? "grabbing" : "grab" }}
+            onPointerDown={() => setIsGrabbing(true)}
+            onPointerUp={() => setIsGrabbing(false)}
+          >
+            <Slider.Range className="absolute h-full" />
+          </Slider.Track>
+          <Slider.Thumb className="opacity-0" />
+        </Slider.Root>
+        <Icons.SpeakerWave className="size-[25px] text-gray-400 hover:!text-white group-hover:text-gray-200 group-hover:transition" />
+      </div>
     </div>
   );
 }
@@ -27,4 +37,7 @@ Old Slider.Thumb classes className="block size-5 rounded-[50%] bg-blue-400".
 active:border-4 active:border-red-500 *:active:border-4 *:active:border-red-500 
 Bug: active works only await from the slider.
 The bug goes a lot deeper: https://buildui.com/recipes/elastic-slider. This is something Sam himself would have to address.
+Removed hover:cursor-grab active:cursor-grabbing because active:cursor-grabbing not working.
+OK. Scale is not height, only visual. So the container was still the h-5 of size-5 from the icons. 
+Problems. Solved. Except for this ONE pixel at the bottom of the slider.
 */
